ea78902ecab001ef13bac644d969ceeb4eee3a2c
khalid192 <karazani2003@gmail.com>
2026-01-27 23:20:56 +0100

    .

--- src/conext/CaluctReducer.jsx
+++ src/conext/CaluctReducer.jsx
@@ -1,19 +1,25 @@
+export default function Freducer(state, action) {
+  switch (action.type) {
+    case "add":
+      console.log(1);
+      return Number(action.data.input1) + Number(action.data.input2);
 
-export default function Freducer(state,action){
-    switch(action.type){
-        case 'add':
-            console.log(1)
-            return Number(action.data.input1) + Number(action.data.input2);
+    case "minus":
+      console.log(2);
+      return Number(action.data.input1) - Number(action.data.input2);
 
-         case 'minus':
-            console.log(2)
-            return Number(action.data.input1) - Number(action.data.input2);
-        
-         case 'multiply':
-            console.log(3)
-            return Number(action.data.input1) * Number(action.data.input2);
+    case "multiply":
+      console.log(3);
+      return Number(action.data.input1) * Number(action.data.input2);
 
-         case 'divide':
-            console.log(4)
-            return Number(action.data.input1) / Number(action.data.input2);}
+    case "divide":
+      console.log(4);
+      if (Number(action.data.input2) === 0) {
+        return 0;
+      }
+      return Number(action.data.input1) / Number(action.data.input2);
+      
+    default:
+      return state;
+  }
 }
