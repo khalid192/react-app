522addc0cee6b5a21e252457a2da7e3f8ccdf257
khalid192 <karazani2003@gmail.com>
2026-02-01 17:38:57 +0100

    .

--- src/reducer/RedurcerList.jsx
+++ src/reducer/RedurcerList.jsx
@@ -2,6 +2,9 @@
 import React, { createContext, useReducer } from "react";
 export const ListContext2 = createContext();
 
+
+
+
 export default function FreducerList(state,action) {
   console.log(state)
   switch (action.type) {
@@ -19,7 +22,7 @@ export default function FreducerList(state,action) {
      { ...itm, title: action.data.inputValue.title, body: action.data.inputValue.body } : itm);
 
     case "Check":
-      
+
       return state.map((itm) =>
         Number(itm.id) === Number(action.data) ? { ...itm, checked: itm.checked ? false : true } : itm
       ) 
